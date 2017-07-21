ecd2934803642cc261a3d579ed6d57260239f86b
throstur <sparog.throstur+github+spam@gmail.com>
2017-07-21 21:03:30 +0000

    amendments

--- README.md
+++ README.md
@@ -13,17 +13,22 @@ Please consider supporting http://eve-files.com/ -- especially if you plan on us
 ## Usage
 
 ```
-usage: influence_stitcher.py [-h] [--originals-dir DIR] [--modified-dir DIR]
-                             DATE [DATE]
+usage: influence_stitcher.py [-h] [--originals-dir PATH] [--modified-dir PATH]
+                             [--output_path FILENAME]
+                             START_DATE [END_DATE]
 
 Create a timelapse of Verite's classic EVE Influence maps
 
 positional arguments:
-  DATE                 A date in the format Ymd (20070809)
-  DATE                 A date in the format Ymd (20170809)
+  START_DATE            A date in the format Ymd (20070809)
+  END_DATE              A date in the format Ymd (20170809); defaults to the
+                        current date
 
 optional arguments:
-  -h, --help           show this help message and exit
-  --originals-dir DIR  Where to save downloaded images
-  --modified-dir DIR   Where to save edited images
+  -h, --help            show this help message and exit
+  --originals-dir PATH  Where to save downloaded images
+  --modified-dir PATH   Where to save edited images
+  --output_path FILENAME
+                        The filename of the final video (and by extension the
+                        file format chosen for ffmpeg)
 ```
--- influence_stitcher.py
+++ influence_stitcher.py
@@ -18,7 +18,7 @@ def download(label, dl_dest):
     file_dest = os.path.join(dl_dest, pic)
     if os.path.isfile(file_dest):
         print 'Skipping ' + label
-        return
+        return file_dest
     target = URL_FORMAT.format(pic)
     print 'Downloading ' + label + '...'
     try:
@@ -26,6 +26,7 @@ def download(label, dl_dest):
     except Exception:
         print 'Could not download ' + label
         pass
+    return file_dest
 
 def edit_image(label, fpath, save_dest, retries_left=3):
     if retries_left < 0:
@@ -45,15 +46,20 @@ def mkdirs(dirs):
         if not os.path.exists(dest):
             os.makedirs(dest)
 
-def main(index, end, dl_dest, save_dest):
+def main(index, end, dl_dest, save_dest, output_path):
     image_number = 0
     while index <= end:
         label = index.strftime(date_fmt) 
-        download(label, dl_dest)
-        edit_image(label, os.path.join(dl_dest, label + '.png'), os.path.join(save_dest, 'img{0:04}.png'.format(image_number)))
+        dl_path = download(label, dl_dest)
+        edit_image(label, dl_path, os.path.join(save_dest, 'img{0:04}.png'.format(image_number)))
         index += timedelta(days=1)
         image_number += 1
-    ffmpeg_cmd = 'ffmpeg -f image2 -i {0} video.avi'.format(os.path.join(save_dest, 'img%04d.png'))
+    # Now extend the last frame a little bit
+    for suffix in range(0, 70):
+        edit_image(label, dl_path, os.path.join(save_dest, 'img{0:04}.png'.format(image_number + suffix)))
+    if os.path.exists(output_path):
+        os.unlink(output_path)
+    ffmpeg_cmd = 'ffmpeg -f image2 -i {0} {1}'.format(os.path.join(save_dest, 'img%04d.png'), output_path)
     result = subprocess.call(ffmpeg_cmd.split(' '))
     if result == 0:
         print '\n'
@@ -65,14 +71,15 @@ if __name__ == '__main__':
     date_fmt = '%Y%m%d'
     today = date.today().strftime(date_fmt)
     parser = argparse.ArgumentParser(description='Create a timelapse of Verite\'s classic EVE Influence maps')
-    parser.add_argument('start', metavar='DATE', help='A date in the format Ymd (20070809)')
-    parser.add_argument('end', metavar='DATE', nargs='?', default=today, help='A date in the format Ymd (20170809); defaults to the current date')
-    parser.add_argument('--originals-dir', metavar='DIR', default='originals', help='Where to save downloaded images')
-    parser.add_argument('--modified-dir', metavar='DIR', default='images', help='Where to save edited images')
+    parser.add_argument('start', metavar='START_DATE', help='A date in the format Ymd (20070809)')
+    parser.add_argument('end', metavar='END_DATE', nargs='?', default=today, help='A date in the format Ymd (20170809); defaults to the current date')
+    parser.add_argument('--originals-dir', metavar='PATH', default='originals', help='Where to save downloaded images')
+    parser.add_argument('--modified-dir', metavar='PATH', default='images', help='Where to save edited images')
+    parser.add_argument('--output_path', metavar='FILENAME', default='video.avi', help='The filename of the final video (and by extension the file format chosen for ffmpeg)')
     args = parser.parse_args()
     start = datetime.strptime(args.start, date_fmt)
     end = datetime.strptime(args.end, date_fmt)
     dl_dest = args.originals_dir
     save_dest = args.modified_dir
     mkdirs([dl_dest, save_dest])
-    main(start, end, dl_dest, save_dest)
+    main(start, end, dl_dest, save_dest, args.output_path)
